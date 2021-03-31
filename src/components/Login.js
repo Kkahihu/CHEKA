2489642f494a86d7534dec10b9fb43e9a91e6189
Kkahihu <kelvinkahihu180226@daystar.ac.ke>
2021-03-31 11:12:15 +0300

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,14 +2,26 @@ import { Button } from '@material-ui/core';
 import React from 'react';
 import logo from '../assets/logo.png';
 import { auth, provider } from '../firebase';
+import { actionTypes } from '../Reducer';
+import { useStateValue } from '../StateProvider';
 
 
 import './Login.css';
 
 function Login() {
+    const [{}, dispatch] = useStateValue();
+
+
     const signIn = () => {
-        auth.signInWithPopup(provider).then(result => 
-            console.log(result)).catch(error => alert(error.message));
+        auth
+        .signInWithPopup(provider)
+        .then((result) => {
+            dispatch({
+                type: actionTypes.SET_USER,
+                user: result.user,
+            });
+        })
+        .catch(error => alert(error.message));
     };
 
 
